07fe797982a924133d439a2ca6402c8b78007fbd
Harish Yerraguntla <harish.yerraguntla@harnstech.com>
2023-03-16 17:49:21 +0530

    changed in index file from master

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+// chnges in index
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
